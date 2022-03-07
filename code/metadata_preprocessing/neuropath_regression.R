1d5d2e88987714546c92fd2a8564806f148816fa
jgockley62 <jgockley62@gmail.com>
2022-03-07 15:42:05 -0800

    Neuropath

--- code/metadata_preprocessing/neuropath_regression.R
+++ code/metadata_preprocessing/neuropath_regression.R
@@ -14,17 +14,16 @@ process <- function( syns, counts, inds ){
   meta <- do.call( rbind, imported)
   
   #Annotate with indv. ID
-  ref <- read.csv(synapser::synGet('syn25792425')$path,header=T) %>%
+  ref <- read.csv(synapser::synGet(inds)$path,header=T) %>%
     tibble::column_to_rownames('specimenID')
   meta$individualID <- ref[meta$specimenID,]$individualID
   
   #Filter to match counts
   counts <- data.table::fread(
-    synapser::synGet('syn25808335')$path, 
+    synapser::synGet(counts)$path, 
     sep = '\t',
     header = T) %>%
     as.data.frame()
-  
   
   counts <- counts[, c('feature',meta$specimenID)]
   return(list(meta = meta,
@@ -188,4 +187,145 @@ synapse_push <- function( object, parentID, used_syns, file_handle, file_names, 
       counts = list( name ='RosMap CogDX Counts',
                      description = 'RosMap Count Data matched to samples with CogDX annotated'))
   )
+
+# Mayo
+  braak <- process( 
+    syns = list(
+      TCX = 'syn26529080',
+      CBE = 'syn26529086'
+    ),
+    counts = 'syn25835094',
+    inds = 'syn26958125'
+  )
+  
+  synapse_push(
+    object = braak,
+    parentID = list(meta = 'syn27554322', counts = 'syn27554273'),
+    used_syns = c('syn26529080', 'syn26529086', 'syn25808335', 'syn25792425'),
+    file_names <- list(meta = 'Mayo_Braak_Metadata', counts = 'Mayo_Braak_Counts'),
+    file_handle = list(meta = 'csv',  counts = 'tsv'),
+    annots = list(meta = 'syn25835093', counts = 'syn25835094'),
+    git_info = list( repo ='Sage-Bionetworks/ampad-rnaseq-reprocessing',
+                     file = 'code/metadata_preprocessing/neuropath_regression.R',
+                     branch = 'main'),
+    activ = list(
+      meta = list( name ='Mayo Braak Metadata',
+                   description = 'Mayo metadata samples with Braak annotated'),
+      counts = list( name ='Mayo Braak Counts',
+                     description = 'Mayo Count Data matched to samples with Braak annotated'))
+  )
+# Thal  
+  thal <- process( 
+    syns = list(
+      TCX = 'syn26529081',
+      CBE = 'syn26529087'
+    ),
+    counts = 'syn25835094',
+    inds = 'syn26958125'
+  )
+  
+  synapse_push(
+    object = thal,
+    parentID = list(meta = 'syn27554322', counts = 'syn27554273'),
+    used_syns = c('syn26529081', 'syn26529087', 'syn25808335', 'syn25792425'),
+    file_names <- list(meta = 'Mayo_Thal_Metadata', counts = 'Mayo_Thal_Counts'),
+    file_handle = list(meta = 'csv',  counts = 'tsv'),
+    annots = list(meta = 'syn25835093', counts = 'syn25835094'),
+    git_info = list( repo ='Sage-Bionetworks/ampad-rnaseq-reprocessing',
+                     file = 'code/metadata_preprocessing/neuropath_regression.R',
+                     branch = 'main'),
+      activ = list(
+      meta = list( name ='Mayo Thal Metadata',
+                   description = 'Mayo metadata samples with Braak annotated'),
+      counts = list( name ='Mayo Thal Counts',
+                     description = 'Mayo Count Data matched to samples with Thal annotated'))
+  )
+  
+# MSBB
+  # Braak  
+  braak <- process( 
+    syns = list(
+      PHG = 'syn26525353',
+      STG = 'syn26525356',
+      IFG = 'syn26525347',
+      FP = 'syn26525337'
+    ),
+    counts = 'syn25872241',
+    inds = 'syn26525356'
+  )
+  
+  synapse_push(
+    object = braak,
+    parentID = list(meta = 'syn27554333', counts = 'syn27554296'),
+    used_syns = c('syn26525353', 'syn26525356', 'syn26525347', 'syn26525337', 'syn25872241', 'syn26525356'),
+    file_names <- list(meta = 'MSBB_Braak_Metadata', counts = 'MSBB_Braak_Counts'),
+    file_handle = list(meta = 'csv',  counts = 'tsv'),
+    annots = list(meta = 'syn26436062', counts = 'syn25872241'),
+    git_info = list( repo ='Sage-Bionetworks/ampad-rnaseq-reprocessing',
+                     file = 'code/metadata_preprocessing/neuropath_regression.R',
+                     branch = 'main'),
+    activ = list(
+      meta = list( name ='MSBB Braak Metadata',
+                   description = 'MSBB metadata samples with Braak annotated'),
+      counts = list( name ='MSBB Braak Counts',
+                     description = 'MSBB Count Data matched to samples with Braak annotated'))
+  )
+  
+  # Cerad  
+  cerad <- process( 
+    syns = list(
+      PHG = 'syn26525354',
+      STG = 'syn26525357',
+      IFG = 'syn26525348',
+      FP = 'syn26525341'
+    ),
+    counts = 'syn25872241',
+    inds = 'syn26525356'
+  )
+  
+  synapse_push(
+    object = cerad,
+    parentID = list(meta = 'syn27554333', counts = 'syn27554296'),
+    used_syns = c('', '', '', '', 'syn25872241', 'syn26525356'),
+    file_names <- list(meta = 'MSBB_Cerad_Metadata', counts = 'MSBB_Cerad_Counts'),
+    file_handle = list(meta = 'csv',  counts = 'tsv'),
+    annots = list(meta = 'syn26436062', counts = 'syn25872241'),
+    git_info = list( repo ='Sage-Bionetworks/ampad-rnaseq-reprocessing',
+                     file = 'code/metadata_preprocessing/neuropath_regression.R',
+                     branch = 'main'),
+    activ = list(
+      meta = list( name ='MSBB Cerad Metadata',
+                   description = 'MSBB metadata samples with Cerad annotated'),
+      counts = list( name ='MSBB Cerad Counts',
+                     description = 'MSBB Count Data matched to samples with Cerad annotated'))
+  ) 
+  
+  # CogDx  
+  cogdx <- process( 
+    syns = list(
+      PHG = 'syn26525355',
+      STG = 'syn26525358',
+      IFG = 'syn26525352',
+      FP = 'syn26525346'
+    ),
+    counts = 'syn25872241',
+    inds = 'syn26525356'
+  )
+  
+  synapse_push(
+    object = cogdx,
+    parentID = list(meta = 'syn27554333', counts = 'syn27554296'),
+    used_syns = c('syn26525355', 'syn26525358', 'syn26525352', 'syn26525346', 'syn25872241', 'syn26525356'),
+    file_names <- list(meta = 'MSBB_CogDx_Metadata', counts = 'MSBB_CogDx_Counts'),
+    file_handle = list(meta = 'csv',  counts = 'tsv'),
+    annots = list(meta = 'syn26436062', counts = 'syn25872241'),
+    git_info = list( repo ='Sage-Bionetworks/ampad-rnaseq-reprocessing',
+                     file = 'code/metadata_preprocessing/neuropath_regression.R',
+                     branch = 'main'),
+    activ = list(
+      meta = list( name ='MSBB CogDx Metadata',
+                   description = 'MSBB metadata samples with CogDx annotated'),
+      counts = list( name ='MSBB CogDx Counts',
+                     description = 'MSBB Count Data matched to samples with CogDx annotated'))
+  ) 
   
